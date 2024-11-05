400c05fe7d6c35097b339ddadcb5d1a90d87befe
Fernando <59489587+fercodes@users.noreply.github.com>
2024-11-05 14:31:10 -0400

    Update members

--- src/constants/teamData.js
+++ src/constants/teamData.js
@@ -1,138 +1,121 @@
-import nicole from "../assets/images/nicole.jpg";
-import karen from "../assets/images/karen.jpg";
-import ruben from "../assets/images/ruben.jpg";
-import adan from "../assets/images/adan.jpg";
-import gabriel from "../assets/images/gabriel.jpg";
-import alejandra from "../assets/images/alejandra.jpg";
-import franco from "../assets/images/franco.jpg";
-import fernando from "../assets/images/fernando.jpg";
+import nicole from '../assets/images/nicole.jpg';
+import ruben from '../assets/images/ruben.jpg';
+import adan from '../assets/images/adan.jpg';
+import gabriel from '../assets/images/gabriel.jpg';
+import alejandra from '../assets/images/alejandra.jpg';
+import franco from '../assets/images/franco.jpg';
+import fernando from '../assets/images/fernando.jpg';
 
 const teamData = [
   {
-    name: "Nicole Jimeno Ruiz",
-    position: "COORDINADORA",
+    name: 'Nicole Jimeno Ruiz',
+    position: 'COORDINADORA',
     photo: nicole,
     socialNetworks: [
       {
-        platform: "linkedin",
-        link: "https://www.linkedin.com/in/nicole-jimeno-ruiz/",
+        platform: 'linkedin',
+        link: 'https://www.linkedin.com/in/nicole-jimeno-ruiz/',
       },
       {
-        platform: "instagram",
-        link: "https://www.instagram.com/bolivian_scientist/",
+        platform: 'instagram',
+        link: 'https://www.instagram.com/bolivian_scientist/',
       },
       {
-        platform: "beacons",
-        link: "https://beacons.ai/nicolejimeno",
+        platform: 'beacons',
+        link: 'https://beacons.ai/nicolejimeno',
       },
     ],
   },
   {
-    name: "Karen Losantos-Ramos",
-    position: "CO-COORDINADORA",
-    photo: karen,
-    socialNetworks: [
-      {
-        platform: "linkedin",
-        link: "https://www.linkedin.com/in/karen-losantos-ramos-58307658/",
-      },
-      { platform: "x", link: "https://x.com/KarenLosantos" },
-      {
-        platform: "beacons",
-        link: "https://beacons.ai/karrencha",
-      },
-    ],
-  },
-  {
-    name: "Ruben Dario Jallaza Calla",
-    position: "DIRECTOR CIENCIAS PLANETARIAS",
+    name: 'Ruben Dario Jallaza Calla',
+    position: 'DIRECTOR CIENCIAS PLANETARIAS',
     photo: ruben,
     socialNetworks: [
       {
-        platform: "linkedin",
-        link: "https://www.linkedin.com/in/ruben-jallaza-394943191/",
+        platform: 'linkedin',
+        link: 'https://www.linkedin.com/in/ruben-jallaza-394943191/',
       },
       {
-        platform: "instagram",
-        link: "https://www.instagram.com/rubenjallaza6/",
+        platform: 'instagram',
+        link: 'https://www.instagram.com/rubenjallaza6/',
       },
     ],
   },
   {
-    name: "Adan Ardaya Dominguez",
-    position: "DIRECTOR EXPLORACIÓN ESPACIAL",
+    name: 'Adan Ardaya Dominguez',
+    position: 'DIRECTOR EXPLORACIÓN ESPACIAL',
     photo: adan,
     socialNetworks: [
       {
-        platform: "linkedin",
-        link: "https://www.linkedin.com/in/adan-ardaya-d-13b78b203/",
+        platform: 'linkedin',
+        link: 'https://www.linkedin.com/in/adan-ardaya-d-13b78b203/',
       },
       {
-        platform: "instagram",
-        link: "https://www.instagram.com/adan_ardayad/",
+        platform: 'instagram',
+        link: 'https://www.instagram.com/adan_ardayad/',
       },
     ],
   },
   {
-    name: "Gabriel Jiménez",
-    position: "DIRECTOR EXPLORACIÓN ESPACIAL",
+    name: 'Gabriel Jiménez',
+    position: 'DIRECTOR EXPLORACIÓN ESPACIAL',
     photo: gabriel,
     socialNetworks: [],
   },
   {
-    name: "Arian Alejandra Kuguimiya Ramos",
-    position: "ENCARGADA DISEÑO Y MARKETING",
+    name: 'Arian Alejandra Kuguimiya Ramos',
+    position: 'ENCARGADA DISEÑO Y MARKETING',
     photo: alejandra,
     socialNetworks: [
       {
-        platform: "linkedin",
-        link: "https://www.linkedin.com/in/alejandra-kuguimiya-8ab04a294/",
+        platform: 'linkedin',
+        link: 'https://www.linkedin.com/in/alejandra-kuguimiya-8ab04a294/',
       },
       {
-        platform: "instagram",
-        link: "https://www.instagram.com/arian_akr/",
+        platform: 'instagram',
+        link: 'https://www.instagram.com/arian_akr/',
       },
       {
-        platform: "researchgate",
-        link: "https://www.researchgate.net/profile/Alejandra-Kuguimiya",
+        platform: 'researchgate',
+        link: 'https://www.researchgate.net/profile/Alejandra-Kuguimiya',
       },
     ],
   },
   {
-    name: "Franco Miguel Martinez Bolivar",
-    position: "ENCARGADO COMUNICACIÓN Y REDES SOCIALES",
+    name: 'Franco Miguel Martinez Bolivar',
+    position: 'ENCARGADO COMUNICACIÓN Y REDES SOCIALES',
     photo: franco,
     socialNetworks: [
       {
-        platform: "linkedin",
-        link: "https://www.linkedin.com/in/franco-martinez-a63644161/",
+        platform: 'linkedin',
+        link: 'https://www.linkedin.com/in/franco-martinez-a63644161/',
       },
       {
-        platform: "instagram",
-        link: "https://www.instagram.com/francomartinezbolivar/",
+        platform: 'instagram',
+        link: 'https://www.instagram.com/francomartinezbolivar/',
       },
       {
-        platform: "researchgate",
-        link: "https://www.researchgate.net/profile/Franco-Martinez-Bolivar",
+        platform: 'researchgate',
+        link: 'https://www.researchgate.net/profile/Franco-Martinez-Bolivar',
       },
     ],
   },
   {
-    name: "Jorge Fernando Vasquez Veliz",
-    position: "ENCARGADO PÁGINA WEB",
+    name: 'Jorge Fernando Vasquez Veliz',
+    position: 'ENCARGADO PÁGINA WEB',
     photo: fernando,
     socialNetworks: [
       {
-        platform: "linkedin",
-        link: "https://www.linkedin.com/in/fervasq/",
+        platform: 'linkedin',
+        link: 'https://www.linkedin.com/in/fervasq/',
       },
       {
-        platform: "x",
-        link: "https://x.com/fernandowillbe",
+        platform: 'x',
+        link: 'https://x.com/fernandowillbe',
       },
       {
-        platform: "github",
-        link: "https://github.com/fercodes",
+        platform: 'github',
+        link: 'https://github.com/fercodes',
       },
     ],
   },

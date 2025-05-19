9d0ff76653fb4342d8303841aed428b3c83d52ce
Fernando <59489587+fercodes@users.noreply.github.com>
2025-05-18 23:40:39 -0400

    Add more images to PhotosSection.jsx

--- src/sections/PhotosSection/PhotosSection.jsx
+++ src/sections/PhotosSection/PhotosSection.jsx
@@ -1,6 +1,9 @@
 import PhotoSlider from '../../components/PhotoSlider/PhotoSlider';
 import styles from './photosSection.module.css';
 
+import foto_2024_workshop_1 from '../../assets/images/events/foto_2024_workshop_1.jpeg';
+import foto_2024_workshop_2 from '../../assets/images/events/foto_2024_workshop_2.jpeg';
+import foto_2024_workshop_3 from '../../assets/images/events/foto_2024_workshop_3.jpeg';
 import foto_1 from '../../assets/images/events/foto_1.jpg';
 import foto_2 from '../../assets/images/events/foto_2.jpg';
 import foto_3 from '../../assets/images/events/foto_3.jpg';
@@ -8,6 +11,18 @@ import foto_4 from '../../assets/images/events/foto_4.jpg';
 
 const PhotosSection = () => {
   const images = [
+    {
+      src: foto_2024_workshop_1,
+      alt: 'actividades',
+    },
+    {
+      src: foto_2024_workshop_2,
+      alt: 'actividades',
+    },
+    {
+      src: foto_2024_workshop_3,
+      alt: 'actividades',
+    },
     {
       src: foto_1,
       alt: 'actividades',
